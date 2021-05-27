046d77a300496ba2d4731aba7b5c2b09bd611a7c
VilenaArturovna <vilena-forever@inbox.ru>
2021-05-27 20:24:35 +0600

    fixing cors

--- nodemailer.js
+++ nodemailer.js
@@ -16,14 +16,14 @@ let transporter = nodemailer.createTransport({
     },
 })
 
-/*const corsOptions = {
+const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}*/
+}
 
 
 //app.use(cors())
-//app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 //'https://vilenaarturovna.github.io'
 // 'http://localhost:3000'
 
