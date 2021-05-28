3ac2ba9245666976025f3be6cd31021853285d49
VilenaArturovna <vilena-forever@inbox.ru>
2021-05-28 17:12:09 +0600

    fixing cors

--- nodemailer.js
+++ nodemailer.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json())
 
 // Add headers
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.header('Access-Control-Allow-Credentials', true);
