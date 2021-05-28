f8b9be0cf4fc416a7bce5b71cbd3fd758ea17f64
VilenaArturovna <vilena-forever@inbox.ru>
2021-05-28 22:58:49 +0600

    I hope final commit

--- nodemailer.js
+++ nodemailer.js
@@ -4,8 +4,8 @@ const cors = require('cors')
 const nodemailer = require("nodemailer")
 const bodyParser = require('body-parser')
 
-const smtp_login = process.env.SMTP_LOGIN || 'vilena27arturovna@gmail.com'
-const smtp_password = process.env.SMTP_PASSWORD || 'Integrirovanie27'
+const smtp_login = process.env.SMTP_LOGIN 
+const smtp_password = process.env.SMTP_PASSWORD 
 const port = process.env.PORT || 3010
 
 let transporter = nodemailer.createTransport({
@@ -16,41 +16,18 @@ let transporter = nodemailer.createTransport({
     },
 })
 
-// const corsOptions = {
-//     origin: '*',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
 app.use(cors({
-    origin: 'https://vilenaarturovna.github.io'
+    origin: 'https://vilenaarturovna.github.io/portfolio'
 }))
-
-//app.use(cors())
-//'https://vilenaarturovna.github.io'
-//'http://localhost:3000'
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
-// Add headers
-/*app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});*/
 
 app.get("/", function (req, res) {
     res.send("<h2>Привет Express!</h2>")
 })
 
 app.post('/sendMessage', async function (req, res) {
-
-   /* res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.set('Access-Control-Allow-Credentials', true);*/
-
     const {values} = req.body
 
     const info = await transporter.sendMail({
@@ -64,9 +41,7 @@ app.post('/sendMessage', async function (req, res) {
                     <div>Message: ${values.message}</div>
                </div>`,
     })
-
     res.send()
-
     console.log("Message sent: %s", info.messageId)
 })
 
